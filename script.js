b067751b5183a35dd06e3684d3f2a2c6a64ba761
LeleVaneck10 <leletokamvaneckauriol@gmail.com>
2023-11-09 13:14:21 +0100

    initializing variables

---
+++ script.js
@@ -0,0 +1,12 @@
+// variables we need
+var sp, btn_start, btn_stop, t, ms, h;
+
+// function to initialize variables when the page is loaded
+window.onload = function(){ // The window.onload event in JavaScript is used to execute a particular piece of code or a function after all the content on a web page has finished loading, including images, scripts, and other resources.
+    sp = document.getElementsByClassName('span');
+    btn_start = document.getElementById('start');
+    btn_stop = document.getElementById('stop');
+    t;
+    ms = 0, s = 0, mn = 0, h = 0;
+}
+
